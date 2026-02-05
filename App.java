ad6289e42809f0d8988573ef6254c8fb9ceed8bf
rbvfn profissional <rbvfn.profissional@gmail.com>
2026-02-05 11:21:39 -0300

    desenvolvimento do código para recebimento do HTML da web

--- App.java
+++ App.java
@@ -1,5 +1,22 @@
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
 public class App {
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws IOException, InterruptedException{
+
+        URI targetUri = URI.create(args[0]);
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                                         .uri(targetUri)
+                                         .GET()
+                                         .build();
+
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        String recivedHtml = response.body();
+
+        System.out.printf("\nHTML recebido da página: %s", recivedHtml);
     }
 }
