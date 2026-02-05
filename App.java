d5fde29ecc05e87c9afcce07c38e68166897b27a
rbvfn profissional <rbvfn.profissional@gmail.com>
2026-02-05 12:05:46 -0300

    Desenvolvimento do código para analisar arquivo html de teste

--- App.java
+++ App.java
@@ -1,22 +1,33 @@
 import java.io.IOException;
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.charset.StandardCharsets;
+//import java.net.URI;
+//import java.net.http.HttpClient;
+//import java.net.http.HttpRequest;
+//import java.net.http.HttpResponse;
 
 public class App {
     public static void main(String[] args) throws IOException, InterruptedException{
 
-        URI targetUri = URI.create(args[0]);
-        HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder()
-                                         .uri(targetUri)
-                                         .GET()
-                                         .build();
+        //URI targetUri = URI.create(args[0]);
+        //HttpClient client = HttpClient.newHttpClient();
+        //HttpRequest request = HttpRequest.newBuilder()
+        //                                 .uri(targetUri)                    // code to recive the HTML
+        //                                 .GET()
+        //                                 .build();
 
-        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-        String recivedHtml = response.body();
+        //HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        //String recivedHtml = response.body();
 
-        System.out.printf("\nHTML recebido da página: %s", recivedHtml);
+        Path htmlPath = Path.of("test.html");
+        String htmlContent = Files.readString(htmlPath, StandardCharsets.UTF_8);
+
+        System.out.println("\n" + htmlContent + "\n");
+
+
+
+
+        //System.out.printf("\nHTML recebido da página: %s", recivedHtml);
     }
 }
